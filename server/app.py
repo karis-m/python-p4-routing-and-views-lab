41e0c00951d7cc617af9f4e2142f77b98ddba268
karis-m <moses.kamau@student.moringaschool.com>
2023-09-19 12:26:21 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -4,5 +4,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/print/<string:parameter>')
+def print_parameter(parameter):
+    return f'<h1>The captured parameter is: {parameter}</h1>'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = list(range(parameter))
+    return f'<h1>{numbers}</h1>'
